34ccadfef83f3fe623131916b968a1073122e723
Nikki W <86427717+anikki1@users.noreply.github.com>
2022-08-09 17:07:49 -0400

    multiple

---
+++ holidayList.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+const nav = document.getElementById('navigation')
+nav.innerHTML = '<ul> <li> <a href="index.html"> home </a> </li> <li> settings</li> <li> <a href="profile.html"> profile </a></li></ul>'
+})
