7486e4431279f02f5b9bbef9f4b2e494714ad897
Nikki W <86427717+anikki1@users.noreply.github.com>
2022-10-12 19:14:05 -0400

    nav bar

--- holidayList.js
+++ holidayList.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
 const nav = document.getElementById('navigation')
-nav.innerHTML = '<ul> <li> <a href="index.html"> Home </a> </li> <li> <a href="profile.html"> profile </a></li> <li> <a href="list.html"> List </a></li> <li> <a href="groups.html"> Groups </a></li>  <li> <a href="settings.html"> Settings </a> </li></ul>'
+nav.innerHTML = '<ul> <li> <a href="index.html"> Home </a> </li> <li> <a href="profile.html"> Profile </a></li> <li> <a href="list.html"> List </a></li> <li> <a href="groups.html"> Groups </a></li>  <li> <a href="settings.html"> Settings </a> </li></ul>'
 })
