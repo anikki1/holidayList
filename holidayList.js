94b2d7e22cf3a6c95d62141ca757b4d0f16cedc4
Nikki W <86427717+anikki1@users.noreply.github.com>
2022-10-23 18:18:15 -0400

    js movement

--- holidayList.js
+++ holidayList.js
@@ -5,40 +5,56 @@ nav.innerHTML = '<ul> <li> <a href="index.html"> Home </a> </li> <li> <a href="p
 const images = ['/media/butternut.jpg', '/media/dogs.jpg', '/media/hanukkah.jpg', '/media/holiday.jpg', '/media/snowman.jpg', '/media/whiteElephant.jpg']
 let imageIndex = 0
 
-
-
+let imgArray = []
 
 
 const carousel = document.getElementsByClassName('carousel')[0] 
-showCarousel()
+images.forEach(image => {
+    const img = document.createElement('img')
+    img.src = image
+    img.className = 'carouselPic'
+    imgArray.push(img)
+    carousel.appendChild(img)
+    img.style = 'left: 800px'
+})
 
+imgArray[0].style = 'left: 10px'
+
+
+
+/*showCarousel()*/
 
 
 
 const next = document.getElementById('next')
 next.onclick = function() {
-    if (imageIndex < images.length - 1) {imageIndex++
-        showCarousel()
-    
+    if (imageIndex < images.length - 1) {
+        showCarousel(true)
     } }
+
+
 const previous = document.getElementById('previous')
 previous.onclick = function () {
     if (imageIndex > 0) {
-        imageIndex--
-        showCarousel()
+        showCarousel(false)
     }
 }
 
 
-function showCarousel () {
-    carousel.innerHTML = `<img class="carouselPic" src="${images[imageIndex]}">`
-
-
+function showCarousel (right) {
+ //   carousel.innerHTML = `<img class="carouselPic" src="${images[imageIndex]}">` //
+        imgArray[imageIndex].style = 'left: 800px'
+        imageIndex+= (right ? 1 : -1)
+        let currentLeft = 10
+        
+        imgArray[imageIndex].style = `left: ${currentLeft}`
+        
+       
 }
-
 
 
 
 
 })
+
 
